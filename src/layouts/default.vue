8018f18a460eb3d2811542aecc4d0df58eaccc0c
tmwatchanan <tmwatchanan@gmail.com>
2018-04-07 17:26:57 +0700

    Deploy to GitHub Pages

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -17,7 +17,7 @@
         </q-btn>
 
         <q-toolbar-title>
-          Quasar App
+          CMU Hungry
           <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
         </q-toolbar-title>
       </q-toolbar>
